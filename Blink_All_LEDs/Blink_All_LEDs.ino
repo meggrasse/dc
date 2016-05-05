4c4cae6c2c3543996f9d5fbd8c25e1fe21c76eb4
Meg Grasse <meggrasse@gmail.com>
2016-05-05 18:15:07 -0500

    break implemented

--- Blink_All_LEDs/Blink_All_LEDs.ino
+++ Blink_All_LEDs/Blink_All_LEDs.ino
@@ -7,38 +7,54 @@ bool ledState = LOW;
 
 void setup() 
 {
-  pinPeripheral(BLUE_LED, PIO_TIMER);
-  pinPeripheral(12, PIO_TIMER);
-  pinMode(BLUE_LED, OUTPUT);
-  pinMode(12, OUTPUT);
-  //pinMode(11, OUTPUT);
-  pinMode(RX_LED, OUTPUT);
-  pinMode(TX_LED, OUTPUT);
-  digitalWrite(RX_LED, HIGH);
-  digitalWrite(TX_LED, HIGH);
- // digitalWrite(12, HIGH);
-//  digitalWrite(BLUE_LED, HIGH);
+ pinMode(RX_LED, OUTPUT);
+ pinMode(TX_LED, OUTPUT);
+ digitalWrite(RX_LED, HIGH);
+ digitalWrite(TX_LED, HIGH);
+ 
+ //h bridge 1
+ pinPeripheral(BLUE_LED, PIO_PWM);
+ pinPeripheral(11, PIO_PWM);
+ pinMode(BLUE_LED, OUTPUT);
+ pinMode(12, OUTPUT);
+ pinMode(11, OUTPUT); 
+ digitalWrite(12, HIGH);
+
+ //h bridge 2
+  pinPeripheral(8, PIO_TIMER);
+  pinPeripheral(6, PIO_TIMER);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  digitalWrite(7, LOW);
+ 
 }
 
 void loop() 
 {
-  // Toggle RX and TX LED's
-  ledState = !ledState;
-  digitalWrite(RX_LED, ledState);
-  digitalWrite(TX_LED, !ledState);
+ // Toggle RX and TX LED's
+ ledState = !ledState;
+ digitalWrite(RX_LED, ledState);
+ digitalWrite(TX_LED, !ledState);
 
-//   Ramp the blue LED up:
-  for (int i=0; i<256; i++)
-  {
-    analogWrite(BLUE_LED, 255);
-    analogWrite(12, 255);
-  }
-  // Ramp the blue LED down:
-//  for (int i=255; i>=0; i--)
-//  {
-//    analogWrite(BLUE_LED, 0);
-//    analogWrite(12,  i);
-//    delay(2);
-//  }
-}
+ // Ramp the PWM waves up:
+    
+   //h bridge 1
+   analogWrite(11, 255);
+   analogWrite(BLUE_LED, 255);
+   //analogWrite(12, 255);
 
+   //h bridge 2
+    analogWrite(8, 255);
+    analogWrite(6, 255);
+
+
+ // Ramp the PWM waves down:
+    delay(1000);
+    //break
+    analogWrite(11, 0);
+    analogWrite(8,  0);
+    delay(1000);
+
+  
+}// end main
