8ee61b7f4d6889b3d14ca75184b5f41c118da571
Meg Grasse <meggrasse@gmail.com>
2016-05-06 20:11:25 -0500

    probably goes in a box

--- Blink_All_LEDs/Blink_All_LEDs.ino
+++ Blink_All_LEDs/Blink_All_LEDs.ino
@@ -7,54 +7,105 @@ bool ledState = LOW;
 
 void setup() 
 {
- pinMode(RX_LED, OUTPUT);
- pinMode(TX_LED, OUTPUT);
- digitalWrite(RX_LED, HIGH);
- digitalWrite(TX_LED, HIGH);
- 
- //h bridge 1
- pinPeripheral(BLUE_LED, PIO_PWM);
- pinPeripheral(11, PIO_PWM);
- pinMode(BLUE_LED, OUTPUT);
- pinMode(12, OUTPUT);
- pinMode(11, OUTPUT); 
- digitalWrite(12, HIGH);
+  pinMode(RX_LED, OUTPUT);
+  pinMode(TX_LED, OUTPUT);
+  digitalWrite(RX_LED, HIGH);
+  digitalWrite(TX_LED, HIGH);
+   
+  //h bridge 1
+  pinPeripheral(BLUE_LED, PIO_TIMER);
+  pinPeripheral(11, PIO_TIMER);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT); 
 
- //h bridge 2
+  //h bridge 2
   pinPeripheral(8, PIO_TIMER);
   pinPeripheral(6, PIO_TIMER);
   pinMode(8, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(6, OUTPUT);
-  digitalWrite(7, LOW);
- 
 }
 
 void loop() 
 {
- // Toggle RX and TX LED's
- ledState = !ledState;
- digitalWrite(RX_LED, ledState);
- digitalWrite(TX_LED, !ledState);
-
- // Ramp the PWM waves up:
+  // Toggle RX and TX LED's
+  ledState = !ledState;
+  digitalWrite(RX_LED, ledState);
+  digitalWrite(TX_LED, !ledState);
     
-   //h bridge 1
-   analogWrite(11, 255);
-   analogWrite(BLUE_LED, 255);
-   //analogWrite(12, 255);
-
-   //h bridge 2
-    analogWrite(8, 255);
-    analogWrite(6, 255);
-
-
- // Ramp the PWM waves down:
-    delay(1000);
-    //break
-    analogWrite(11, 0);
-    analogWrite(8,  0);
-    delay(1000);
-
+  moveStraight();
   
-}// end main
+  delay(1000);
+  
+  shortBreak();
+  
+  delay(1000);
+
+  turnRight();
+
+  delay(1000);
+
+  shortBreak();
+  
+  delay(1000);
+  
+}
+
+void moveStraight() {
+  //h bridge 1 cw
+  analogWrite(BLUE_LED, 255);
+  digitalWrite(12, HIGH);
+  analogWrite(11, 255);
+
+  //h bridge 2 ccw
+  analogWrite(8, 255);
+  digitalWrite(7, LOW);
+  analogWrite(6, 255);
+}
+
+void turnLeft() {
+   //h bridge 1 cw
+  analogWrite(BLUE_LED, 255);
+  digitalWrite(12, HIGH);
+  analogWrite(11, 255);
+
+  //h bridge 2 cw
+  analogWrite(8, 255);
+  digitalWrite(7, HIGH);
+  analogWrite(6, 255);
+}
+
+void turnRight() {
+  //h bridge 1 cw
+  analogWrite(BLUE_LED, 255);
+  digitalWrite(12, LOW);
+  analogWrite(11, 255);
+
+  //h bridge 2 cw
+  analogWrite(8, 255);
+  digitalWrite(7, LOW);
+  analogWrite(6, 255);
+}
+
+
+void moveBackward() {
+   //h bridge 1 ccw
+  analogWrite(BLUE_LED, 255);
+  digitalWrite(12, LOW);
+  analogWrite(11, 255);
+
+  //h bridge 2 cw
+  analogWrite(8, 255);
+  digitalWrite(7, HIGH);
+  analogWrite(6, 255);
+}
+
+void shortBreak() {
+   //h bridge 1 break
+  analogWrite(11, 0);
+
+  //h bridge 2 break
+  analogWrite(8,  0);
+}
+
