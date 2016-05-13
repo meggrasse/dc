9f55fe19edbba9ddab646290ffcf4b12cb7d8c4f
Meg Grasse <meggrasse@gmail.com>
2016-05-13 13:19:54 -0500

    most updated code, simple smart decisions

--- laser_move/laser_move.ino
+++ laser_move/laser_move.ino
@@ -17,30 +17,35 @@ Servo servo_1;  // initializes servo object
 #define ECHO 5
 
 const int pwmSpeed = 70;
-const int ultrasonicDistance = 50;
-const int lineSensor = 800;
+const int ultrasonicDistance = 20;
+const int lineSensor = 1000;
 
+//intialize sensors
 double laser_value = 0;
+double ultraDistance = 0;
+int lineLeft = 0;
+int lineRight = 0;
+double duration = 0;
+double distance = 0;
+
 bool ledState = LOW;
 
-//ultrasonic init
-long duration;
-long distance;
+//intialize speed
+int leftSpeed = pwmSpeed;
+int rightSpeed = pwmSpeed;
 
 // servo initialization code
 uint16_t minpulse = 0.5;
 uint16_t maxpulse = 2.5;
 int servoPosition = 10;
 int increment = 10;
-int LEFT_SPEED = pwmSpeed;
-int RIGHT_SPEED = pwmSpeed;
 int count = 0;
 
 void setup() {
   servo_1.attach(SERVO_PIN, minpulse, maxpulse); // lets the servo pin know it is operating a servo 
   // Serial output code
-  //SerialUSB.begin(9600); // serial initialize
-  //while(!SerialUSB) ;  // wait for serial to connect 
+//  SerialUSB.begin(9600); // serial initialize
+//  while(!SerialUSB) ;  // wait for serial to connect 
 
   pinMode(15, OUTPUT);   // sets the pin as output
   bool ledState = 0;
@@ -71,7 +76,6 @@ void setup() {
   pinMode(ECHO, INPUT);
 
   moveStraight(pwmSpeed, pwmSpeed);
-
 }
 
 void loop() 
@@ -79,73 +83,59 @@ void loop()
   ledState = !ledState;
   digitalWrite(RX_LED, ledState);
   digitalWrite(TX_LED, !ledState);
-  
+
+  //start servo
   scanServo(servoPosition, increment, &servoPosition, &increment);
-  
-  moveStraight(RIGHT_SPEED, LEFT_SPEED);
 
+  //start moving
+  moveStraight(rightSpeed, leftSpeed);
+
+  //gather sensor data
   laser_value = checkLaser(LASER_PIN);
+  ultraDistance = checkUltra();
+  lineLeft = analogRead(A1);
+  lineRight = analogRead(A2);
 
-  if (laser_value >= 100){
-    if (servoPosition >= 90 ){
-      //speed up the right motor
-      LEFT_SPEED = LEFT_SPEED + 20;
-      
-    }
-
-    else {
+  //make decision
+  if (lineLeft > lineSensor || lineRight > lineSensor) { 
+//  SerialUSB.println("line detected");
+//  SerialUSB.println(lineLeft);
+//  SerialUSB.println(lineRight);
+  moveBackward(pwmSpeed, pwmSpeed);
+  delay(2000);
+  //turnRight();  
+  }
+  else if (distance < ultrasonicDistance && distance > 0) {
+ //   SerialUSB.println("obstacle detected");
+    turnRight();
+  }
+  else if (laser_value >= 100 && servoPosition >= 90) {
+   //    SerialUSB.println("laser detected 1");
+       //speed up the right motor
+      leftSpeed = leftSpeed + 20;
+  }
+  else if (laser_value >= 100 & servoPosition < 90) {
+  //    SerialUSB.println("laser detected 2");
       //speed up the left motor
-      RIGHT_SPEED = RIGHT_SPEED + 20;
-    }
-     
-    
-    }// end laser val if
-
-    if (count >= 8) {
-      RIGHT_SPEED = pwmSpeed;
-      LEFT_SPEED = pwmSpeed;
-      count = 0;
-      }
-
-    count++;
+      rightSpeed = rightSpeed + 20;
+  }
+   
+//    if (count >= 8) {
+//      rightSpeed = pwmSpeed;
+//      leftSpeed = pwmSpeed;
+//      count = 0;
+//      }
+//
+//    count++;
   
-  // check line sensor left
-  //int line_val_left = analogRead(LINE_SENSOR_LEFT_PIN);
-
-  // check line sensor right
-  //int line_val_right = analogRead(LINE_SENSOR_RIGHT_PIN);
-
-  // check ultrasonic sensor
-  //int ulta_value = checkUlta();
-  
-  // get laser position
-  //int laser_value = checkLaser(LASER_PIN);
-  
-  // make decision - is obstacle close? is robot on table edge 
-  
-  // execute!
-
-  // short delay
-
-  
-  //if (SerialUSB.available()) {
-  //laser_value = checkLaser(LASER_PIN);
-  //SerialUSB.println(laser_value);
-  
-  
-  //SerialUSB.println("Servo_position: ");
-  //SerialUSB.println(servoPosition);
-  //SerialUSB.println("Servo_increment: ");
-  //SerialUSB.println(increment);
-  //} // end if statement
-
 } // end loop
 
-double checkUlta() {
-  digitalWrite(TRIG, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
+double checkUltra() {
+  //send ultrasonic signal and determines distance
+  digitalWrite(TRIG, LOW); 
+  delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10); // Added this line
+  delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   duration = pulseIn(ECHO, HIGH);
   distance = (duration/2) / 29.1; 
@@ -219,13 +209,13 @@ void turnRight() {
 void moveBackward(int right_pwm, int left_pwm) {
    //h bridge 1 ccw
   analogWrite(BLUE_LED, 255);
-  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
   analogWrite(11, right_pwm);
 
   //h bridge 2 cw
-  analogWrite(8, 255);
-  digitalWrite(7, LOW);
-  analogWrite(6, left_pwm);
+  analogWrite(8, left_pwm);
+  digitalWrite(7, HIGH);
+  analogWrite(6, 255);
 } // end move backwards
 
 void shortBreak() {
